7c4116f98ba2814ed814f65ae210e29453f47412
Vadym Tereshchuk <link.biz@ukr.net>
2021-06-08 17:43:36 +0300

    Added task0105

---
+++ src/main/java/com/fara0n/task/task01/task0105/Solution.java
@@ -0,0 +1,16 @@
+package com.fara0n.task.task01.task0105;
+
+/*
+Объявляем переменные
+Напиши программу, которая в методе main объявляет такие переменные:
+name типа String, age типа int и city типа String.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        String name;
+        int age;
+        String city;
+    }
+}

0e4adfd798fb5c5fc899cdf45b3d01b61dcf84af
Vadym Tereshchuk <link.biz@ukr.net>
2021-06-08 22:54:46 +0300

    Added task0106

---
+++ src/main/java/com/fara0n/task/task01/task0106/Solution.java
@@ -0,0 +1,14 @@
+package com.fara0n.task.task01.task0106;
+
+/*
+Баги и фичи
+Напиши программу, которая выводит на экран надпись:
+Это не баг, это фича.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        System.out.println("Это не баг, это фича");
+    }
+}

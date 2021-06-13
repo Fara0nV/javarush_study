b6a633e0cb6e39633103c0d67a41d3c8ac7f4249
Vadym <link.biz@ukr.net>
2021-06-13 21:25:34 +0300

    Added package task0117 with Solution class

---
+++ src/main/java/com/fara0n/task/task01/task0117/Solution.java
@@ -0,0 +1,22 @@
+package com.fara0n.task.task01.task0117;
+
+/*
+Внеси изменения в программу, чтобы выводился на экран текст:
+Coding in java
+Используй переменные.
+
+Требования:
+•	Должно быть объявлено 3 переменных.
+•	Всем переменным должны быть присвоены значение сразу после создания.
+•	Команду вывода текста на экран изменять нельзя.
+•	Программа должна выводить текст "Coding in java".
+ */
+
+public class Solution {
+    public static void main(String[] args) {
+        String string1 = "Coding";
+        String string2 = "in";
+        String string3 = "java";
+        System.out.println(string1 + " " + string2 + " " + string3);
+    }
+}

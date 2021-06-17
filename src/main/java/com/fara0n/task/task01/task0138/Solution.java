de7259a9487d469d8a3b045ec702bc2ad3e92da9
Vadym <link.biz@ukr.net>
2021-06-17 15:12:54 +0300

    Added package 0138 with solution class

---
+++ src/main/java/com/fara0n/task/task01/task0138/Solution.java
@@ -0,0 +1,24 @@
+package com.fara0n.task.task01.task0138;
+
+/*
+В методе main напиши код, чтобы в результате выполнения программы было выведено твое любимое стихотворение.
+В стихотворении должно быть не меньше, чем 4 строчки. Не убирай вывод строчки "Мое любимое стихотворение:".
+
+Требования:
+•	Программа должна выводить текст.
+•	Перед стихом должна быть выведена строка "Мое любимое стихотворение:".
+•	Выводимый текст должен состоять не меньше, чем из пяти строк (пустые строки - это тоже строки).
+•	В выводимом тексте должно быть не менее, чем 50 символов.
+ */
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Мое любимое стихотворение:");
+        //напишите тут ваш код
+        System.out.println();
+        System.out.println("Take me to the magic of the moment");
+        System.out.println("On a glory night");
+        System.out.println("Where the children of tomorrow dream away");
+        System.out.println("In the wind of change");
+    }
+}

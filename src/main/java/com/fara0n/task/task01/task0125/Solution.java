b0946fe3e44898cae2b717d680b45bbffa3f3769
Vadym <link.biz@ukr.net>
2021-06-17 09:58:49 +0300

    Added package 0125 with solution class(modified)

--- src/main/java/com/fara0n/task/task01/task0125/Solution.java
+++ src/main/java/com/fara0n/task/task01/task0125/Solution.java
@@ -16,10 +16,8 @@ public class Solution {
         int a = 5;
         //int z = 18;
         int d = 18;
-        int c = a +d;
+        int c = a + d;
         String b = " is better than ";
-        System.out.println(a + b +c);
+        System.out.println(a + b + c);
     }
-
-
 }

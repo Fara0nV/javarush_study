1ea56f1d6ee2d101d7f3fce21d7156815177e657
Vadym <link.biz@ukr.net>
2021-06-17 20:08:24 +0300

    Added class for file copy

---
+++ src/main/java/com/fara0n/task/task01/additional/file_copy/FileCopy.java
@@ -0,0 +1,20 @@
+package com.fara0n.task.task01.additional.file_copy;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileCopy {
+    public static void main(String[] args) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream("d:\\data.txt");
+        FileOutputStream fileOutputStream = new FileOutputStream("d:\\result.txt");
+
+        while (fileInputStream.available() > 0) {
+            int data = fileInputStream.read();
+            fileOutputStream.write(data);
+        }
+
+        fileInputStream.close();
+        fileInputStream.close();
+    }
+}
